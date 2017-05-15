7287546767f651497a1b33459c4b39c8c4acbe50
Vanessa Hamar <hamar@cc.in2p3.fr>
2017-05-15 13:26:51 +0200

    cl7

--- manifests/zero/install.pp
+++ manifests/zero/install.pp
@@ -14,17 +14,42 @@ class cvmfs::zero::install (
   # We grab latest kernel unless we have an aufs one running
   # already.
 
-  unless $::kernelrelease  =~ /^.*aufs.*/ {
-    notify{'An aufs kernel is not, install, upgrade, reboot until an aufs kernel is running':}
-  }
 
-  package{'kernel':
-    ensure  => $cvmfs_kernel_version,
-    require => Yumrepo['cvmfs-kernel'],
-  }
-  package{'aufs2-util':
-    ensure  => $cvmfs_aufs2_version,
-    require => Yumrepo['cvmfs-kernel'],
-  }
+
+
+  
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        '6': {
+          unless $::kernelrelease  =~ /^.*aufs.*/ {
+              notify{'An aufs kernel is not, install, upgrade, reboot until an aufs kernel is running':}
+          }
+          package{'kernel':
+            ensure  => $cvmfs_kernel_version,
+            require => Yumrepo['cvmfs-kernel'],
+          }
+          package{'aufs2-util':
+            ensure  => $cvmfs_aufs2_version,
+            require => Yumrepo['cvmfs-kernel'],
+          }
+        }
+        '7': {
+          notify{'CL7 aufs is not necessary':}
+        }
+        default: {
+          fail("${::operatingsystem}: version ${::operatingsystemmajrelease} not tested yet")
+        }
+      }
+    }
+    'Debian': {
+      fail("osfamily `${::osfamily}` not tested")
+    }
+    default: {
+      fail("osfamily `${::osfamily}` not supported")
+    }
+  }  
+  
+  
   ensure_packages('httpd', { ensure => present, } )
 }
