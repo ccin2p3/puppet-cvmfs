85212584625069bff7c1a0fed801c3d8ad0a74d1
Vanessa Hamar <hamar@cc.in2p3.fr>
2017-05-15 17:42:18 +0200

    not tested

--- manifests/zero/yum.pp
+++ manifests/zero/yum.pp
@@ -55,7 +55,7 @@ class cvmfs::zero::yum (
           }
         }
         '7': {
-          notify{'CL7 aufs is not necessary':}
+          notify{'CL7 aufs is not necessary again':}
         }
         default: {
           fail("${::operatingsystem}: version ${::operatingsystemmajrelease} not tested yet")
